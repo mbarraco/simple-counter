282ef2db880df3257f2032296d20a57ed08ef624
m <mm@ms-MacBook-Pro.local>
2018-03-13 01:03:35 -0300

    Add style to scoreboard

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+Bootstrap(app)
 
 login = LoginManager(app)
 login.login_view = 'login'
